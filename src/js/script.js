3ca3aaadbf683dfc1f5b24d048a4a66d378646a1
Sergey <example@gmail.com>
2020-09-25 10:35:52 +0700

    add project

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+// console.log('привет');
+// alert(1);
+// let que = prompt("ты лох?","");
+
+// console.log(que);
+/* for( let i = 1; i < 10; i++)
+{
+    console.log(i);
+} */
+// const num = 99;
+// if(num < 70){
+//     console.log('ну мало,епта')
+// } else if(num>100) {
+//     console.log('вот ты гигант')
+// } else {
+//     console.log('падёт')
+// }
+function calc (a,b) {
+    console.log(a + b);
+}
+calc(43,123);
